891052291b5590cec7690589d1fbd1562eac4c8a
deren21 <109866900+deren21@users.noreply.github.com>
2023-10-27 15:20:57 +0700

    <Separate Folder & add Lecture 09 demo code>

---
+++ Lecture 09/ComponentBasedModel/ComputeRectangleArea/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace ComputeRectangleArea
+{
+    public class Rectangle
+    {
+        //Definisi class attrbutes
+        private double panjang;
+        private double lebar;
+
+        //Constructor
+        public Rectangle(double length, double width)
+        {
+            panjang = length;
+            lebar = width;
+        }
+
+        //Fungsi untuk menghitung luas persegi panjang
+        public double CalculateArea()
+        {
+            double luasPersegiPanjang;
+            luasPersegiPanjang = panjang * lebar;
+            return luasPersegiPanjang;
+        }
+    }
+}
---
+++ Lecture 09/ComponentBasedModel/UsingComponent/Program.cs
@@ -0,0 +1,14 @@
+﻿using ComputeRectangleArea;
+
+class Program
+{
+    public static void Main()
+    {
+        double panjang = 15.0;
+        double lebar = 10.0;
+        Rectangle persegiPanjang = new Rectangle(panjang, lebar);
+        double luas = persegiPanjang.CalculateArea();
+        Console.WriteLine($"Luas dari persegi panjang" + $"dengan panjang {panjang} dan " + $"lebar {lebar} adalah {luas}");
+        Console.ReadLine();
+    }
+}
